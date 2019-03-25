8382a6f155b07062f7d5b9b6bc4f2d5850c0874e
JGarcia <JGarcia.x@icloud.com>
2019-03-25 13:44:48 +1000

    everything done

---
+++ simulatedAnn.py
@@ -0,0 +1,99 @@
+import numpy as np
+import math
+import copy
+import time
+
+def SA(initialBoard, n):
+    e = 2.71828
+    temp = 999
+    coolingRate = 0.003
+
+    # Get the fitness for initialBoard
+    fitnessScore = fitness(initialBoard)
+    newBoard = copy.deepcopy(initialBoard)
+
+    #while h > 0:
+    while temp > 1:
+            # Pick a random Queen's position
+            ranQueen = np.random.randint(low=0, high=n)
+            current = initialBoard[ranQueen]
+            newPosition = current
+
+            # Pick a random position to move the
+            # Queen to
+            while newPosition == current:
+                newPosition = np.random.randint(low=0, high=n)
+
+            newBoard[ranQueen] = newPosition
+
+            # Check Fitness
+            newFitScore = fitness(newBoard)
+            r = np.random.uniform(0,1)
+            p = e**-(newFitScore - fitnessScore)/temp
+            if fitnessScore >= newFitScore:
+                # Move Queen to newPosition
+                initialBoard = copy.deepcopy(newBoard)
+                fitnessScore = newFitScore
+            elif p < r:
+                initialBoard = copy.deepcopy(newBoard)
+                fitnessScore = newFitScore
+
+            temp *= 1-coolingRate
+
+    return (initialBoard, newFitScore)
+
+def fitness(state):
+    fitnessScore = 0
+    for i in range(len(state)-1):
+        for j in range(i+1, len(state)):
+            #checking for queens in the same column
+            if state[i] == state[j]:
+                fitnessScore += 1
+            #checking for dia left
+            if state[i]-(j-i) == state[j]:
+                fitnessScore += 1
+            # Check for dia right
+            if state[i]+(j-i) == state[j]:
+                fitnessScore +=1
+    return fitnessScore
+
+def printBoard(finishedBoard):
+    for i in range(len(finishedBoard)):
+        for j in range(len(finishedBoard)):
+            if j != finishedBoard[i]:
+                print("-", end = " ")
+            else:
+                print("Q", end = " ")
+        print()
+
+def main():
+
+    #n = int(input("Get n: "))
+    n = 4
+    h = 9999
+
+    #-----------------------
+    # Start time to solution
+    #-----------------------
+    start = time.time()
+    time.process_time()
+
+    while h > 0:
+        #random board generator
+        initialBoard = list(np.random.randint(low=0, high=n, size=n))
+
+        # Simmulated Anneling
+        bestBoard, h = SA(initialBoard, n)
+
+    printBoard(bestBoard)
+    print(h)
+
+    #-----------------------------
+    # Time that taken to solution
+    #-----------------------------
+    elapsed = time.time() - start
+    math.ceil(elapsed)
+    print("Time from start to solution: ", round(elapsed, 1))
+
+
+main()
